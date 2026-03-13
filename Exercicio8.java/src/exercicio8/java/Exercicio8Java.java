dfdd361db5e497fa78e60f9ebac606f3e8be13e6
murilo167 <murilopasso95@gmail.com>
2026-03-13 14:43:20 -0300

    Add files via upload

---
+++ Exercicio8.java/src/exercicio8/java/Exercicio8Java.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio8.java;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Exercicio8Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        CarroAluguel carro = new CarroAluguel("Onix", "Chevrolet", 2022, 150.00);
+
+        carro.setValorDiaria(170.00);
+
+        carro.exibirCarro();}
+    }
+    
+
