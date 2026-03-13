6b2245ad0e0476e5d415674792faf2599fb3b55a
murilo167 <murilopasso95@gmail.com>
2026-03-13 14:33:01 -0300

    Add files via upload

---
+++ Exercicio10/exercicios1java/src/exercicios1java/Exercicios1java.java
@@ -0,0 +1,24 @@
+
+package exercicios1java;
+
+ 
+public class Exercicios1java {
+
+
+    public static void main(String[] args) {
+      
+        
+        // Criando objeto
+     pessoa1 objetopessoa1 = new pessoa1();
+
+        // Atribuindo valores
+       objetopessoa1.nome = "Carlos Vinicius";
+      objetopessoa1.idade = 30;
+
+        // Chamando método
+        objetopessoa1.apresentar();
+    }
+}
+    
+    
+
---
+++ Exercicio10/exercicios1java/src/exercicios1java/pessoa1.java
@@ -0,0 +1,18 @@
+
+package exercicios1java;
+
+
+ 
+public class pessoa1 {
+   
+
+    // Atributos
+    String nome;
+    int idade;
+
+    // Método
+    public void apresentar() {
+        System.out.println("Olá, meu nome é " + nome + " e tenho " + idade + " anos.");
+    }
+}
+
